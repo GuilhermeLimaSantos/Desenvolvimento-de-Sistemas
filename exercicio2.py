3f4fbababc1673ff007e373b51ba171d78f541d1
Guilherme H. l. dos Santos <125158701+GuilhermeLimaSantos@users.noreply.github.com>
2024-02-27 16:32:21 -0300

    Create exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,12 @@
+num1 = 10
+num2 = 20
+
+total = num1 + num2 # realizando o calculo de soma
+
+print("A soma do número {} + {} = {}".format{num1, num2, total})
+
+# Quais são os operadores em python?
+# + (soma)
+# - (subtração)
+# * (multiplicação)
+# / (divisão)
