220988364b32e363d5f9ae8aff620f3217f9f673
Guilherme H. l. dos Santos <125158701+GuilhermeLimaSantos@users.noreply.github.com>
2024-02-27 13:52:54 -0300

    Create exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,16 @@
+# Como se chama uma variável?
+
+idade = 16
+nome = "Guilherme"
+preco = 29.10
+
+# Como podemos mudar essas variáveis no programa?
+# Função print()
+
+print("A idade é: ", idade)
+
+# Como podemos mostrar variáveis dentro das string?
+
+text = "Vou comprar {} que custa {} real"
+
+print(text.format{"Banana", preco})
